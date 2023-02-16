a52dfaa219ef3749c7af15f4860e4696a357de8a
TStutsman <teagandstutsman@gmail.com>
2023-02-16 15:27:47 -0500

    Added dependencies for Javalin in Java 8

--- BackEnd/datta-backend/src/main/java/com/revature/App.java
+++ BackEnd/datta-backend/src/main/java/com/revature/App.java
@@ -1,29 +1,21 @@
 package com.revature;
 
-import java.net.InetSocketAddress;
+// import java.net.InetSocketAddress;
 
-import com.revature.controller.PostController;
-import com.sun.net.httpserver.HttpServer;
+// import com.revature.controller.PostController;
+// import com.sun.net.httpserver.HttpServer;
 
-/**
- * Hello world!
- */
+import io.javalin.Javalin;
+
 public final class App {
     private App() {
     }
 
-    /**
-     * Says hello to the world.
-     * @param args The arguments of the program.
-     */
     public static void main(String[] args) throws Exception{
-        System.out.println("Starting backend server...");
 
-        HttpServer server = HttpServer.create(new InetSocketAddress(8000), 0);
+        Javalin app = Javalin.create(/*config*/)
+            .get("/", ctx -> ctx.result("Hello World"))
+            .start(8000);
 
-        server.createContext("/posts", new PostController());
-
-        server.setExecutor(null);
-        server.start();
     }
 }

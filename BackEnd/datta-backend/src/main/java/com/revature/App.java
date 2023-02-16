371686e6e65c3755c7cb5f4af098a0a049694e49
TStutsman <teagandstutsman@gmail.com>
2023-02-15 23:38:38 -0500

    Added skeleton to PostController and created PostService(not a controller) as well as created context for URI in App.java

--- BackEnd/datta-backend/src/main/java/com/revature/App.java
+++ BackEnd/datta-backend/src/main/java/com/revature/App.java
@@ -1,5 +1,10 @@
 package com.revature;
 
+import java.net.InetSocketAddress;
+
+import com.revature.controller.PostController;
+import com.sun.net.httpserver.HttpServer;
+
 /**
  * Hello world!
  */
@@ -11,7 +16,14 @@ public final class App {
      * Says hello to the world.
      * @param args The arguments of the program.
      */
-    public static void main(String[] args) {
-        System.out.println("Hello World!");
+    public static void main(String[] args) throws Exception{
+        System.out.println("Starting backend server...");
+
+        HttpServer server = HttpServer.create(new InetSocketAddress(8000), 0);
+
+        server.createContext("/posts", new PostController());
+
+        server.setExecutor(null);
+        server.start();
     }
 }

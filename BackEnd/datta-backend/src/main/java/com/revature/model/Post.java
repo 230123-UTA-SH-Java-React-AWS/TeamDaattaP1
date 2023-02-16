21d41774565cc7168c83bf0defc512b89a0f6192
booleons <booleons@gmail.com>
2023-02-16 15:45:46 -0500

    Liked post/comment info

--- BackEnd/datta-backend/src/main/java/com/revature/model/Post.java
+++ BackEnd/datta-backend/src/main/java/com/revature/model/Post.java
@@ -3,9 +3,23 @@ package com.revature.model;
 public class Post {
     private String content;
     private java.sql.Date tStamp;
+    private boolean isLiked;
+    private java.sql.Date likeTStamp;
     //private int userid
     //private int id
 
+    public boolean isLiked() {
+        return isLiked;
+    }
+    public void setLiked(boolean isLiked) {
+        this.isLiked = isLiked;
+    }
+    public java.sql.Date getLikeTStamp() {
+        return likeTStamp;
+    }
+    public void setLikeTStamp(java.sql.Date likeTStamp) {
+        this.likeTStamp = likeTStamp;
+    }
     public String getContent() {
         return content;
     }

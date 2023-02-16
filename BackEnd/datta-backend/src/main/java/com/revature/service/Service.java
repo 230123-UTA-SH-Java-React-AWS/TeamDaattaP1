d65f61e1eabad43c2a6e608d98dab293f060508e
booleons <booleons@gmail.com>
2023-02-16 15:41:25 -0500

    Revert "Models added"
    This reverts commit aa8c3f1fb5520c7a51670124699de00d9a11fef3.

--- BackEnd/datta-backend/src/main/java/com/revature/service/Service.java
+++ BackEnd/datta-backend/src/main/java/com/revature/service/Service.java
@@ -27,8 +27,8 @@ public class Service {
         //ObjectMapper mapper = new ObjectMapper();
 
         /*try {
-            LoginCred newLogin = mapper.readValue(jsonString, LoginCred.class);
-            repo.loginUser(newLogin);
+            User newUser = mapper.readValue(jsonString, User.class);
+            repo.loginUser(newUser);
         } */
 
     }

e7e489afda89a68bf984dff199ea11c1f7888bb5
ItsLaz <yesimandriy@gmail.com>
2023-02-20 13:37:14 -0600

    added win95 and styled components dependencies, setup win95 base styles and theme provider

--- FrontEnd/datta-frontend/src/index.tsx
+++ FrontEnd/datta-frontend/src/index.tsx
@@ -4,7 +4,6 @@ import { Provider } from "react-redux";
 import { store } from "./redux/store";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
-import "./index.css";
 import {
   Route,
   RouterProvider,
@@ -12,17 +11,53 @@ import {
   createRoutesFromElements,
 } from "react-router-dom";
 
+// WIN95/STYLED COMPONENTS
+import { styleReset } from "react95";
+import { createGlobalStyle, ThemeProvider } from "styled-components";
+
+/* Pick a theme of your choice */
+import original from "react95/dist/themes/original";
+
+/* Original Windows95 font (optional) */
+// uses fonts.d.ts file to declare the fonts as modules for typescript
+import ms_sans_serif from "react95/dist/fonts/ms_sans_serif.woff2";
+import ms_sans_serif_bold from "react95/dist/fonts/ms_sans_serif_bold.woff2";
+
+//REACT
 const container = document.getElementById("root")!;
 const root = createRoot(container);
 
+// ROUTER
 const router = createBrowserRouter(
   createRoutesFromElements(<Route path="/" element={<App />}></Route>)
 );
 
+// WIN 95 styles
+const GlobalStyles = createGlobalStyle`
+  @font-face {
+    font-family: 'ms_sans_serif';
+    src: url('${ms_sans_serif}') format('woff2');
+    font-weight: 400;
+    font-style: normal
+  }
+  @font-face {
+    font-family: 'ms_sans_serif';
+    src: url('${ms_sans_serif_bold}') format('woff2');
+    font-weight: bold;
+    font-style: normal
+  }
+  body, input, select, textarea {
+    font-family: 'ms_sans_serif';
+  }
+`;
+
 root.render(
   <React.StrictMode>
     <Provider store={store}>
-      <RouterProvider router={router} />
+      <GlobalStyles />
+      <ThemeProvider theme={original}>
+        <RouterProvider router={router} />
+      </ThemeProvider>
     </Provider>
   </React.StrictMode>
 );

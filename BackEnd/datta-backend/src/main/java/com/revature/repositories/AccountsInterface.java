8476a8ded0768f6a5230a2f430131cc142d11c3c
TStutsman <teagandstutsman@gmail.com>
2023-02-25 20:26:40 -0500

    minor changes

--- BackEnd/datta-backend/src/main/java/com/revature/repositories/AccountsInterface.java
+++ BackEnd/datta-backend/src/main/java/com/revature/repositories/AccountsInterface.java
@@ -1,9 +1,6 @@
 package com.revature.repositories;
 
-import java.util.List;
-
 import com.revature.model.Account;
-import com.revature.model.LoginCred;
 
 public interface AccountsInterface {
     public Account getAccount(int id);

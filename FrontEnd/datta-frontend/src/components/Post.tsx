6d34a6fc561735c4865953561487e1bf97072a27
TStutsman <teagandstutsman@gmail.com>
2023-02-25 20:52:26 -0500

    Added Post and Comments Tabs to Post.tsx

--- FrontEnd/datta-frontend/src/components/Post.tsx
+++ FrontEnd/datta-frontend/src/components/Post.tsx
@@ -1,7 +1,20 @@
-import { Button, Toolbar, Window, WindowContent, WindowHeader } from "react95";
+import { useState } from "react";
+import { Button, Frame, GroupBox, Tab, TabBody, Tabs, TextInput, Toolbar, Window, WindowContent, WindowHeader } from "react95";
 import styled from "styled-components";
 
 function Post() {
+  const [state, setState] = useState({
+    activeTab: 0,
+  });
+
+  const handleChange = (
+    value: number,
+    event: React.MouseEvent<HTMLButtonElement>
+  ) => {
+    setState({ activeTab: value });
+  };
+  const { activeTab } = state;
+
   return (
     <>
       <PostWindow resizable className="window" style={{ marginBottom: 20 }}>
@@ -21,12 +34,30 @@ function Post() {
           </Button>
         </Toolbar>
         <WindowContent>
-          <p>
-            Lorem ipsum dolor sit amet consectetur, adipisicing elit.
-            Consectetur totam voluptates delectus mollitia expedita modi
-            repudiandae ex deserunt voluptate, deleniti praesentium aliquid, id
-            odio quam nobis ea nulla soluta porro?
-          </p>
+          <Tabs value={activeTab} onChange={handleChange}>
+            <Tab value={0}>Post</Tab>
+            <Tab value={1}>Comments</Tab>
+          </Tabs>
+          <TabBody style={{ height: 600 }}>
+            {activeTab === 0 && (
+              <p>
+                Lorem ipsum dolor sit amet consectetur, adipisicing elit.
+                Consectetur totam voluptates delectus mollitia expedita modi
+                repudiandae ex deserunt voluptate, deleniti praesentium aliquid, id
+                odio quam nobis ea nulla soluta porro?
+              </p>
+            )}
+            {activeTab === 1 && (
+              <GroupBox className="groupBox" label="User 1">
+                <p>
+                  Lorem ipsum dolor sit amet consectetur, adipisicing elit.
+                  Consectetur totam voluptates delectus mollitia expedita modi
+                  repudiandae ex deserunt voluptate, deleniti praesentium aliquid, id
+                  odio quam nobis ea nulla soluta porro?
+                </p>
+              </GroupBox>
+            )}
+          </TabBody>
         </WindowContent>
       </PostWindow>
     </>

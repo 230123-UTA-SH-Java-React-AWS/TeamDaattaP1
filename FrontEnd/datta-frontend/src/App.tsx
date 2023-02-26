d7069ef1911cab00a9fee69e2f6bd715d9074347
TStutsman <teagandstutsman@gmail.com>
2023-02-25 21:03:33 -0500

    checking if our posts will populate

--- FrontEnd/datta-frontend/src/App.tsx
+++ FrontEnd/datta-frontend/src/App.tsx
@@ -10,22 +10,41 @@ export interface testPokeData {
   name: string;
   url: string;
 }
+export interface testPostResponse {
+  count: number;
+  next: string;
+  previous: null;
+  results: testPost[];
+}
+export interface testPost {
+  userid: number;
+  content: string;
+}
 
 function App() {
   // testing useAxios hook to fetch data
-  const [loading, data, error, request] = useAxios<testPokeApiResponse>({
+  // const [loading, data, error, request] = useAxios<testPokeApiResponse>({
+  //   method: "GET",
+  //   url: "https://pokeapi.co/api/v2/pokemon?limit=10&offset=0",
+  // });
+  // console.log(data);
+  // if (loading) return <p>Loading...</p>;
+  // if (error !== "") return <p>{error}</p>;
+  // if (!data) return <p>Data was null</p>;
+
+  const [loading, data, error, request] = useAxios<testPostResponse>({
     method: "GET",
-    url: "https://pokeapi.co/api/v2/pokemon?limit=10&offset=0",
-  });
-  console.log(data);
-  if (loading) return <p>Loading...</p>;
-  if (error !== "") return <p>{error}</p>;
-  if (!data) return <p>Data was null</p>;
+    url: "http://localhost:8000/post"
+  })
+
+  if(loading) return <p>Loading...</p>;
+  if(error !== "") return <p>{error}</p>;
+  if(!data) return <p>Data was null</p>;
 
   return (
     <>
       {data.results.map((p) => (
-        <h3 key={p.name}>{p.name}</h3>
+        <h3 key={p.content}>{p.content}</h3>
       ))}
     </>
   );
